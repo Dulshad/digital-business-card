ffe8693abbe4f4199388f8ab878a88fba0384f3d
Dulshad Faraj <dulshadf@gmail.com>
2024-10-21 01:17:35 +0200

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Info from './components/info/Info.jsx'
 import About from './components/about/About.jsx'
 import Interests from './components//interests/Interests.jsx'
